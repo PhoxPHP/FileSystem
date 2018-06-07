d4dd6a4e8c35b28a1d822ae03bd36708dbd43295
Peter2962 <taiwop3@gmail.com>
2018-06-07 03:51:50 +0200

    Updating Uploader class

--- src/File/Uploader/Uploader.php
+++ src/File/Uploader/Uploader.php
@@ -22,35 +22,83 @@
 
 namespace Kit\FileSystem\File\Uploader;
 
+use Closure;
+use StdClass;
+use RuntimeException;
+
 class Uploader
 {
 
 	/**
-	* @var 		$upload_directory
+	* @var 		$destinationFolder
 	* @access 	protected
 	*/
-	protected 	$upload_directory;
+	protected 	$destinationFolder;
 
 	/**
-	* @var 		$max_file_size
+	* @var 		$destinationFolders
 	* @access 	protected
 	*/
-	protected 	$max_file_size;
+	protected 	$destinationFolders = [];
 
 	/**
-	* @var 		$file_types
+	* @var 		$maxFileSize
 	* @access 	protected
 	*/
-	protected 	$file_types;
+	protected 	$maxFileSize = 500;
 
 	/**
-	* @param 	$options <Array> 
+	* @var 		$fileTypes
+	* @access 	protected
+	*/
+	protected 	$fileTypes = [];
+
+	/**
+	* @var 		$filename
+	* @access 	protected
+	*/
+	protected 	$filename = null;
+
+	/**
+	* @var 		$filenameGenerator
+	* @access 	protected
+	*/
+	protected 	$filenameGenerator  = null;
+
+	/**
+	* @var 		$handle
+	* @access 	protected
+	*/
+	protected 	$handle = null;
+
+	/**
+	* @var 		$haltProcess
+	* @access 	protected
+	*/
+	protected 	$haltProcess = false;
+
+	/**
+	* @var 		$error
+	* @access 	protected
+	*/
+	protected 	$error;
+
+	/**
+	* @var 		$uploadedFiles
+	* @access 	protected
+	*/
+	protected 	$uploadedFiles = [];
+
+	/**
+	* Sets the file request name.
+	*
+	* @param 	$handle <String>
 	* @access 	public
-	* @return 	void
+	* @return 	<void>
 	*/
-	public function __construct(Array $options=[])
+	public function setHandle(String $handle)
 	{
-
+		$this->handle = $handle;
 	}
 
 	/**
@@ -58,23 +106,23 @@ class Uploader
 	*
 	* @param 	$filename <String>
 	* @access 	public
-	* @return 	void
+	* @return 	<void>
 	*/
 	public function setNewFilename(String $filename)
 	{
-
+		$this->filename = $filename;
 	}
 
 	/**
-	* Sets new filename for uploaded file.
+	* Sets filename generator when uploading multiple files.
 	*
-	* @param 	$filename <String>
+	* @param 	$generator <String>
 	* @access 	public
-	* @return 	void
+	* @return 	<void>
 	*/
-	public function setNewFilenamesGenerator(String $filename)
+	public function setNewFilenamesGenerator(String $generator)
 	{
-
+		$this->filenameGenerator = $generator;
 	}
 
 	/**
@@ -82,11 +130,11 @@ class Uploader
 	*
 	* @param 	$size <Integer>
 	* @access 	public
-	* @return 	void
+	* @return 	<void>
 	*/
-	public function setMaximumFileSize(int $size=500)
+	public function setMaxFileSize(int $size=500)
 	{
-
+		$this->maxFileSize = $size;
 	}
 
 	/**
@@ -94,11 +142,197 @@ class Uploader
 	*
 	* @param 	$fileTypes <Array>
 	* @access 	public
-	* @return 	void
+	* @return 	<void>
 	*/
 	public function setFileTypes(Array $fileTypes)
 	{
+		$this->fileTypes = $fileTypes;
+	}
 
+	/**
+	* Sets the upload directory to use.
+	*
+	* @param 	$destinationFolder <String>
+	* @access 	public
+	* @return 	<void>
+	*/
+	public function setDestinationFolder(String $destinationFolder=null)
+	{
+		$this->destinationFolder = $destinationFolder;
+	}
+
+	/**
+	* Sets upload directory based on different file types provided.
+	*
+	* @param 	$criteriaTypes <Array>
+	* @access 	public
+	* @return 	<void>
+	*/
+	public function setDestinationFolderByFileTypes(Array $criteriaTypes=[])
+	{
+		$this->destinationFolders = $criteriaTypes;
+	}
+
+	/**
+	* Returns error encountered when processing uploads.
+	*
+	* @access 	public
+	* @return 	Array
+	*/
+	public function getError()
+	{
+		return $this->error;
+	}
+
+	/**
+	* Returns an array of uploaded files.
+	*
+	* @access 	public
+	* @return 	Array
+	*/
+	public function getUploadedFiles() : Array
+	{
+		return $this->uploadedFiles;
+	}
+
+	/**
+	* Halts the current upload process.
+	*
+	* @access 	public
+	* @return 	<void>
+	*/
+	public function haltProcess()
+	{
+		$this->haltProcess = true;
+	}
+
+	/**
+	* Initialize upload.
+	*
+	* @param 	$strict <Boolean> | If this is set to true, exceptions will be thrown if an error
+	* 			occurs while uploading.
+	* @param 	$beforeCallback <Closure> | Closure function that is run before a file is uploaded.
+	* @param 	$afterCallback <Closure> | Closure function that is run aftr a file is uploaded.
+	* @access 	public
+	* @return 	<void>
+	*/
+	public function upload(Bool $strict=false, Closure $beforeCallback=null, Closure $afterCallback=null)
+	{
+		if ($this->handle && isset($_FILES[$this->handle])) {
+			$files = $_FILES[$this->handle];
+			if (is_array($files['name'])) {
+				// If multiple files, loop through the files and process each file.
+				for($i = 0; $i < count($files['name']); $i++) {
+					if ($this->haltProcess == true) {
+						break;
+					}
+
+					$file = new StdClass();
+					$file->originalName = $files['name'][$i];
+					$file->size = $files['size'][$i];
+					$file->tmpName = $files['tmp_name'][$i];
+					$file->errorNumber = $files['error'][$i];
+					$file->type = $files['type'][$i];
+					$file->newName = ($this->filenameGenerator !== null) ? $this->filenameGenerator . $i : $file->originalName;
+					$file->hasIndex = true;
+					$file->index = $i;
+
+					$this->processUpload($file, $strict, $beforeCallback, $afterCallback);
+				}
+			}else{
+				$file = new StdClass();
+				$file->originalName = $files['name'];
+				$file->size = $files['size'];
+				$file->tmpName = $files['tmp_name'];
+				$file->errorNumber = $files['error'];
+				$file->type = $files['type'];
+				$file->newName = ($this->filenameGenerator !== null) ? $this->filenameGenerator . $i : $file->originalName;
+				$file->hasIndex = false;
+
+				$this->processUpload($file, $strict, $beforeCallback, $afterCallback);
+			}
+		}
+	}
+
+	/**
+	* Process file ready to be uploaded.
+	*
+	* @param 	$file <StdClass>
+	* @param 	$strict <Boolean>
+	* @param 	$beforeCallback <Mixed> | If you modified the file object and you would like to use the
+	*			modified file object in the beforeCallback closure, all you need to do is return the object
+	*			in this closure. 
+	* @param 	$afterCallback <Mixed>
+	* @access 	protected
+	* @return 	<void>
+	*/
+	protected function processUpload(StdClass $file, Bool $strict, $beforeCallback, $afterCallback)
+	{
+		if ($beforeCallback instanceof Closure) {
+			$modifiedFile = $beforeCallback($file, $this);
+
+			if ($modifiedFile instanceof StdClass) {
+				$file = $modifiedFile;
+			}
+		}
+
+		if ($this->haltProcess == true) {
+			return;
+		}
+
+		if ($file->errorNumber !== 0) {
+			$this->haltProcess();
+			$this->error = 'File could not be uploaded.';
+
+			if ($strict == true) {
+				throw new RuntimeException($this->error);
+			}
+
+			return;
+		}
+
+		if ($file->size > $this->maxFileSize) {
+			$this->haltProcess();
+			$this->error = 'File size must be greater than ' . $this->formatSize($this->maxFileSize);
+
+			if ($strict == true) {
+				throw new RuntimeException($this->error);
+			}
+
+			return;
+		}
+
+		$destinationFolder = (isset($this->destinationFolders[$file->type])) ? $this->destinationFolders[$file->type] : $this->destinationFolder;
+
+		if (!isset($file->destinationFolder)) {
+			$file->destinationFolder = $destinationFolder;
+		}
+
+		if (!empty($this->errors)) {
+			$this->haltProcess();
+			return;
+		}
+
+		$this->uploadedFiles[] = $file;
+		if ($afterCallback instanceof Closure) {
+			$afterCallback($file, $this);
+		}
+	}
+
+	/**
+	* Formats byte sizes to kilobytes, megabytes, gigabytes and terabytes. 
+	*
+	* @param 	$size <Integer>
+	* @access 	protected
+	* @return 	<Integer>
+	*/
+	protected function formatSize(int $size=0)
+	{
+		$base = log($size) / log(1024);
+		$suffix = array('bytes', 'kb', 'mb', 'gb', 'tb')[floor($base)];
+		
+		$formattedSize = pow(1024, $base - floor($base));
+		return round($formattedSize, 2) . ' ' . $suffix;
 	}
 
 }
